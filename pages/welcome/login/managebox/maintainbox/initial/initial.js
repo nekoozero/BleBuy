bd9b56fa61f865d42dc57058b9999045de8e8d3e
nekoozero <781869463@qq.com>
2018-07-04 14:45:40 +0800

    修改bug：初始化货柜时获取特征值失败

--- pages/welcome/login/managebox/maintainbox/initial/initial.js
+++ pages/welcome/login/managebox/maintainbox/initial/initial.js
@@ -185,155 +185,159 @@ Page({
           return x.congridpass;
         });
         
-
-        let p = new Promise((resolve,reject)=>{
+//这里是针对IOS的手机  IOS写特征值是要先获取一下设备的服务和服务下的特征值  并且这些里面的字母都要大写  android不需要
+        
           //获取此蓝牙设备的服务名
           wx.getBLEDeviceServices({
             deviceId: mac,
             success: function (res) {
               console.log(res);
-            },
-          });
-          resolve();
-        }).then(()=>{
-          //then开始
-          //获得此服务下的特征值
-          wx.getBLEDeviceCharacteristics({
-            deviceId: mac,
-            serviceId: '0000FFF0-0000-1000-8000-00805F9B34FB',
-            success: function (res) {
-              console.log(res);
-              //发第一个数据包
-              wx.writeBLECharacteristicValue({
+                
+              //获取到服务后获得特征值
+              wx.getBLEDeviceCharacteristics({
                 deviceId: mac,
-                //serviceId: '0000ffe0-0000-1000-8000-00805f9b34fb',
-                //characteristicId: '0000ffe1-0000-1000-8000-00805f9b34fb',  //自己的板子
                 serviceId: '0000FFF0-0000-1000-8000-00805F9B34FB',
-                characteristicId: '0000FFF1-0000-1000-8000-00805F9B34FB',  //老师的板子
-                value: sendsupplyArray,
                 success: function (res) {
-                  console.log('第一个发送成功')
-                  util.sleep(1000);
-                  //发第二个数据包
+                  console.log(res);
+                  //发第一个数据包
                   wx.writeBLECharacteristicValue({
                     deviceId: mac,
                     //serviceId: '0000ffe0-0000-1000-8000-00805f9b34fb',
                     //characteristicId: '0000ffe1-0000-1000-8000-00805f9b34fb',  //自己的板子
                     serviceId: '0000FFF0-0000-1000-8000-00805F9B34FB',
                     characteristicId: '0000FFF1-0000-1000-8000-00805F9B34FB',  //老师的板子
-                    value: sendpickArray,
-                    success: function () {
-                      console.log('第二个发送成功');
+                    value: sendsupplyArray,
+                    success: function (res) {
+                      console.log('第一个发送成功')
                       util.sleep(1000);
-                      //把剩下的八个包发送出去,每个包四个格子的密码
-                      let all = [];
-                      all.push(datasupp[0] + datasupp[1] + datasupp[2] + datasupp[3]);
-                      all.push(datasupp[4] + datasupp[5] + datasupp[6] + datasupp[7]);
-                      all.push(datasupp[8] + datasupp[9] + datasupp[10] + datasupp[11]);
-                      all.push(datasupp[12] + datasupp[13] + datasupp[14] + datasupp[15]);
-                      all.push(datasupp[16] + datasupp[17] + datasupp[18] + datasupp[19]);
-                      all.push(datasupp[20] + datasupp[21] + datasupp[22] + datasupp[23]);
-                      all.push(datasupp[24] + datasupp[25] + datasupp[26] + datasupp[27]);
-                      all.push(datasupp[28] + datasupp[29] + '00000000' + '00000000');
+                      //发第二个数据包
+                      wx.writeBLECharacteristicValue({
+                        deviceId: mac,
+                        //serviceId: '0000ffe0-0000-1000-8000-00805f9b34fb',
+                        //characteristicId: '0000ffe1-0000-1000-8000-00805f9b34fb',  //自己的板子
+                        serviceId: '0000FFF0-0000-1000-8000-00805F9B34FB',
+                        characteristicId: '0000FFF1-0000-1000-8000-00805F9B34FB',  //老师的板子
+                        value: sendpickArray,
+                        success: function () {
+                          console.log('第二个发送成功');
+                          util.sleep(1000);
+                          //把剩下的八个包发送出去,每个包四个格子的密码
+                          let all = [];
+                          all.push(datasupp[0] + datasupp[1] + datasupp[2] + datasupp[3]);
+                          all.push(datasupp[4] + datasupp[5] + datasupp[6] + datasupp[7]);
+                          all.push(datasupp[8] + datasupp[9] + datasupp[10] + datasupp[11]);
+                          all.push(datasupp[12] + datasupp[13] + datasupp[14] + datasupp[15]);
+                          all.push(datasupp[16] + datasupp[17] + datasupp[18] + datasupp[19]);
+                          all.push(datasupp[20] + datasupp[21] + datasupp[22] + datasupp[23]);
+                          all.push(datasupp[24] + datasupp[25] + datasupp[26] + datasupp[27]);
+                          all.push(datasupp[28] + datasupp[29] + '00000000' + '00000000');
 
-                      for (let i = 2; i < 10; i++) {
-                        let sendsign = [0, i];
-                        let singleArr = all[i - 2];
-                        for (let i = 0, x = 2; i < 32; i = i + 2, x = x + 1) {
-                          sendsign.push(singleArr.substr(i, 2));
-                        }
+                          for (let i = 2; i < 10; i++) {
+                            let sendsign = [0, i];
+                            let singleArr = all[i - 2];
+                            for (let i = 0, x = 2; i < 32; i = i + 2, x = x + 1) {
+                              sendsign.push(singleArr.substr(i, 2));
+                            }
 
-                        let signArray = new Uint8Array(sendsign.map(function (h) {
-                          return parseInt(h, 16);
-                        })).buffer;    //八个包 单个包的arrayBuffer
-                        console.log();
+                            let signArray = new Uint8Array(sendsign.map(function (h) {
+                              return parseInt(h, 16);
+                            })).buffer;    //八个包 单个包的arrayBuffer
+                            console.log();
 
-                        wx.writeBLECharacteristicValue({
-                          deviceId: mac,
-                          //serviceId: '0000ffe0-0000-1000-8000-00805f9b34fb',
-                          //characteristicId: '0000ffe1-0000-1000-8000-00805f9b34fb',  //自己的板子
-                          serviceId: '0000FFF0-0000-1000-8000-00805F9B34FB',
-                          characteristicId: '0000FFF1-0000-1000-8000-00805F9B34FB',  //老师的板子
-                          value: signArray,
-                          success: function (res) {
-                            console.log('第' + i + '包完成');
-
-
-
-                          },
-                          fail: function (res) {
-                            console.log(res);
-                            return;
-                          }
-                        })
-                        util.sleep(1000);
-
-                      }
+                            wx.writeBLECharacteristicValue({
+                              deviceId: mac,
+                              //serviceId: '0000ffe0-0000-1000-8000-00805f9b34fb',
+                              //characteristicId: '0000ffe1-0000-1000-8000-00805f9b34fb',  //自己的板子
+                              serviceId: '0000FFF0-0000-1000-8000-00805F9B34FB',
+                              characteristicId: '0000FFF1-0000-1000-8000-00805F9B34FB',  //老师的板子
+                              value: signArray,
+                              success: function (res) {
+                                console.log('第' + i + '包完成');
 
 
-                      wx.request({
-                        url: 'https://www.jsqckj.cn/btunlockweb/containers/updatecontainerstatus',
-                        data: {
-                          conid: conId
-                        },
-                        method: "POST",
-                        header: {
-                          'content-type': 'application/x-www-form-urlencoded'
-                        },
-                        success: function (res) {
-                          wx.hideLoading();
-                          if (res.data.code == 200) {
-                            wx.showToast({
-                              title: '初始化成功',
-                              icon: 'success',
-                              duration: 2000
-                            });
-                            wx.navigateTo({
-                              url: '../maintainbox',
+
+                              },
+                              fail: function (res) {
+                                console.log(res);
+                                return;
+                              }
                             })
+                            util.sleep(1000);
+
                           }
+
+
+                          wx.request({
+                            url: 'https://www.jsqckj.cn/btunlockweb/containers/updatecontainerstatus',
+                            data: {
+                              conid: conId
+                            },
+                            method: "POST",
+                            header: {
+                              'content-type': 'application/x-www-form-urlencoded'
+                            },
+                            success: function (res) {
+                              wx.hideLoading();
+                              if (res.data.code == 200) {
+                                wx.showToast({
+                                  title: '初始化成功',
+                                  icon: 'success',
+                                  duration: 2000
+                                });
+                                wx.navigateTo({
+                                  url: '../maintainbox',
+                                })
+                              }
+                            }
+                          })
+                        }, fail: function (res) {
+                          console.log(res);
+                          wx.showModal({
+                            title: '提示',
+                            content: '失败了',
+                          });
+                          wx.hideLoading();
+                          that.setData({
+                            loading: false,
+                            disabled: false
+                          });
+                        }, complete: function (ers) {
+
+
                         }
                       })
-                    }, fail: function (res) {
-                      console.log(res);
+                    },
+                    fail: function (res) {
                       wx.showModal({
                         title: '提示',
                         content: '失败了',
                       });
                       wx.hideLoading();
+                      console.log(res);
                       that.setData({
                         loading: false,
-                        disabled: false
+                        disabled: false,
+                        msg: res.errMsg
                       });
-                    }, complete: function (ers) {
-
 
                     }
                   })
-                },
-                fail: function (res) {
-                  wx.showModal({
-                    title: '提示',
-                    content: '失败了',
-                  });
-                  wx.hideLoading();
-                  console.log(res);
-                  that.setData({
-                    loading: false,
-                    disabled: false,
-                    msg: res.errMsg
-                  });
-
+                }, fail: function (err) {
+                  console.log(err);
                 }
-              })
-            },fail:function(err){
-              console.log(err);
-            }
+              });
+          
+             //写入结束
+            },
           });
-          return ;
-        })
+         
+        
       }
     });
    
   }
 })
+
+
+
+
